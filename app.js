c564cbc9faa0857609e6fa9cf7b3dcf1568a4e57
Eloy Chacon <eloychacon@miguayaba.com>
2023-09-11 09:30:22 -0500

    Prueba 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hola Wilmer desde Azure')
 })
 
 app.listen(port, () => {
