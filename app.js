8e68d6d796943e3fc0b7464d3545800e91b677c5
Eloy Chacon <eloychacon@miguayaba.com>
2023-09-10 22:14:03 -0500

    Carga Inicial

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const servidor = http.createServer((req, res) => {
 });
 
 const puerto = 80; // Escucha en el puerto 80
-const direccion = 'mgpanelv12.azurewebsites.net/'; // Reemplaza 'tudominio.com' con tu URL personalizada
+const direccion = 'mgpanelv12.azurewebsites.net'; // Reemplaza 'tudominio.com' con tu URL personalizada
 
 servidor.listen(puerto, direccion, () => {
   console.log(`Servidor en funcionamiento en https://${direccion}/`);
