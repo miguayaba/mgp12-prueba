e6da72a8c8e3f6e6f555ce06c049d92d5c26c20d
Eloy Chacon <eloychacon@miguayaba.com>
2023-09-11 09:10:56 -0500

    Prueba

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-const http = require('https');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-  // Aquí puedes agregar código para manejar el error, como registrarlo o finalizar la aplicación.
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-process.on('uncaughtException', (error) => {
-  console.error('Uncaught Exception:', error);
-  // Aquí puedes agregar código para manejar el error, como registrarlo o finalizar la aplicación.
-});
-
-const servidor = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hola Mundo Azure\n');
-});
-
-const direccion = '20.119.0.42'; // Escuchar en todas las interfaces de red
-
-servidor.listen(direccion, () => {
-  console.log(`Servidor en funcionamiento en https://${direccion}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
