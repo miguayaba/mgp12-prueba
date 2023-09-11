7e0bc66b7d15cd84ed14b36e2d3ea02eb49eb32d
Eloy Chacon <eloychacon@miguayaba.com>
2023-09-10 22:22:59 -0500

    Carga Inicial

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const servidor = http.createServer((req, res) => {
 });
 
 const puerto = 80; // Puerto 80 para HTTP
-const direccion = '0.0.0.0'; // Escuchar en todas las interfaces de red
+const direccion = '20.119.0.42'; // Escuchar en todas las interfaces de red
 
 servidor.listen(puerto, direccion, () => {
   console.log(`Servidor en funcionamiento en https://${direccion}:${puerto}/`);
