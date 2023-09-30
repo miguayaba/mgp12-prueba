c85c57a885e376771ba2433a35ad045eb5fe9a10
Eloy Chacon <eloychacon@miguayaba.com>
2023-09-29 20:05:16 -0500

    Actualizacion

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hola Wilmer desde Azure')
+  res.send('Hola Mundo desde Azure')
 })
 
 app.listen(port, () => {
