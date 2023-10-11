59f8409b8711f7a1aee3914924c094c306134019
Eloy Chacon <eloychacon@miguayaba.com>
2023-10-11 09:38:46 -0500

    google cloud

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hola Mundo desde Azure')
+  res.send('<h1> Hola Mundo... Estamos conectados desde Google Cloud</h1>')
 })
 
 app.listen(port, () => {
